9749d429261943fba34e80ba794870364ca25ce2
Alecjandro <alec.buitrago20@gmail.com>
2025-04-02 11:51:15 -0500

    Se agrega chakra ui a los productos destacados

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import styles from'./App.module.css'
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import Shortcut from './components/Shortcut/Shortcut';
-import Featuredproducts from './components/Featuredproducts/Featuredproducts';
+import Fproducts from './components/Featuredproducts/Featuredproducts';
 import Footer from './components/Footer/Footer';
+import { Provider } from "./components/ui/provider"
 function App() {
 
   return (
+    <Provider>
     <div className={styles.container}> 
       <Navbar />
       <Header />
       <Shortcut />
-      <Featuredproducts />
+      <Fproducts />
       <Footer />
     </div>
+    </Provider>
   )
 }
 
--- src/components/Featuredproducts/Featuredproducts.jsx
+++ src/components/Featuredproducts/Featuredproducts.jsx
@@ -1,29 +1,52 @@
-import styles from './Featuredproducts.module.css';
-import { FiShoppingCart } from 'react-icons/fi';
+import { Button, Card, Image, Text } from "@chakra-ui/react"
+import { FiShoppingCart } from "react-icons/fi";
+const FeaturedProducts = () => {
+  return (
 
-const Featuredproducts = () => {
-    return (
-    <div className={styles.containerCard}>
-    
-      <div className={styles.featuredCards}>
-        <div className={styles.cardImage}>
-              <img src="" alt="" />
-        </div>
-        <div className={styles.cardBody}>
-              <h4 className={styles.cardTitle}>titylo</h4>
-              <p className={styles.cardDescription}>fsdkfkjshfkdñngkjdhfgkd</p>
-              <p className={styles.cardPrice}>$100</p>
-        </div>
-        <div className={styles.cardFooter}>
-            <FiShoppingCart className={styles.cardCartIcon} />
-            <button>Comprar</button>
-        </div>
-      </div>
+    <Card.Root  display="flex"  flexWrap="wrap" gap="5px"
+              m="25px"overflow="hidden" variant="ghost"borderRadius="30px 15px"  
+              maxW="200px" p="10px"  maxH="460px"
+              boxShadow="0px 6px 8px rgba(15, 15, 15, 0.5)" 
+              transition="transform 0.2s ease-in-out" _hover={{ transform: "scale(1.05)" }}
+              bg="#F3F4F6" 
+    >
 
-      
+      <Image w="100%" h="150px"  objectFit="cover" borderRadius="15px"
+        src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        alt="Green double couch with wooden legs"
+      />
 
-    </div>
-    );
+      <Card.Body gap="2" p="10px 5px" maxW="160px" > 
+
+        <Card.Title mt="5px" fontSize="18px" fontWeight="bold" mb="5px">Titulo</Card.Title>
+
+        <Card.Description mt="5px" fontSize="14px" 
+        mb="10px" wordBreak="break-word"
+        whiteSpace="normal" overflowWrap="break-word">
+          This sofa is perfect for modern tropical spaces.
+        </Card.Description> 
+
+        <Text fontSize="18px" fontWeight="bold" mt="5px" textAlign="right" color="#1E3A8A">
+          $450.000
+        </Text>
+      </Card.Body>
+
+      <Card.Footer mt="5px" gap="2" justifyContent="flex-end" p="10px 5px">
+
+         <FiShoppingCart size="25px" 
+                        color="#10B981" 
+                        cursor="pointer" 
+                        transition="background 0.3s"
+                        _hover={{ bg: "#059669" }}
+  
+                        />
+        
+        <Button  bg="#10B981" color="white" border="none" 
+                p="8px 12px" borderRadius="10px" cursor="pointer" 
+                fontSize="14px" transition="background 0.3s"_hover={{ bg: "#059669" }} 
+                >Comprar</Button>
+      </Card.Footer>
+    </Card.Root>
+  );
 }
-
-export default Featuredproducts;
+export default FeaturedProducts;
