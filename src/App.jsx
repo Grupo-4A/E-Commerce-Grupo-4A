333837094670428d834d72f11f04a6a9a18cd05d
Onhofre <onofrebernalsantiago@gmail.com>
2025-04-29 12:41:28 -0500

    Se crean las rutas para navegar por la web

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
-import styles from'./App.module.css'
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
-import Shortcut from './components/Shortcut/Shortcut';
-import Fproducts from './components/Featuredproducts/Featuredproducts';
-import Footer from './components/Footer/Footer';
-import { Provider } from "./components/ui/provider"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './layouts/mainLayout';
+
 function App() {
 
   return (
-    <Provider>
-    <div className={styles.container}> 
-      <Navbar />
-      <Header />
-      <Shortcut />
-      <Fproducts />
-      <Footer />
-    </div>
-    </Provider>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
---
+++ src/layouts/mainLayout.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import styles from '../App.module.css';
+import Navbar from '../components/Navbar/Navbar';
+import Header from '../components/Header/Header';
+import Shortcut from '../components/Shortcut/Shortcut';
+import Fproducts from '../components/Featuredproducts/Featuredproducts';
+import Footer from '../components/Footer/Footer';
+import { Provider } from "../components/ui/provider";
+
+export default function Home() {
+  return (
+    <Provider>
+      <div className={styles.container}>
+        <Navbar />
+        <Header />
+        <Shortcut />
+        <Fproducts />
+        <Footer />
+      </div>
+    </Provider>
+  );
+}
