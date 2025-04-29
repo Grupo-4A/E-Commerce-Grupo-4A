66077b0429611a34ba7dee065bad4957d5783b17
Onhofre <onofrebernalsantiago@gmail.com>
2025-04-29 14:12:21 -0500

    Creacion de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './layouts/mainLayout';
+import { Inicio } from "./layouts/login/login";
 
 function App() {
 
   return (
       <BrowserRouter>
         <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Inicio />} />
         </Routes>
       </BrowserRouter>
   )
